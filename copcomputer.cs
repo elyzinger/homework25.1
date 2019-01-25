b4cb91a70da2642896e627fb0fcf6670ff6f4354
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-01-25 18:13:50 +0200

    Add files via upload

---
+++ copcomputer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mech_1
+{
+    class Computer
+    {
+        public string model;
+        public int price;
+        public int numberOfProcessors;
+        public float screenSize;
+        public bool isOn;
+        public int tellmetheprice()
+        {
+            return price;
+        }
+        public float tellscreensize()
+        {
+            return screenSize;
+        }
+       public void turnon()
+        {
+            Console.WriteLine("turning on");
+        }
+        public void turnoff()
+        {
+            Console.WriteLine("shutting down");
+        }
+        public void addprocessor()
+        {
+
+        }
+
+    }
+}
